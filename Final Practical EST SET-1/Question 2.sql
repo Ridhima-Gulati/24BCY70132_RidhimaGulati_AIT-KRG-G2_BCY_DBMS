cff0e0c99be572c83477aae00d93a4d4fbd53a56
Ridhima-Gulati <Ridhima.gulati.asr@gmail.com>
2026-04-28 10:50:54 +0530

    Add files via upload

---
+++ Final Practical EST SET-1/Question 2.sql
@@ -0,0 +1,23 @@
+
+CREATE OR REPLACE FUNCTION check_stock_before_insert()
+RETURNS TRIGGER AS $$
+DECLARE
+    available_stock INT;
+BEGIN
+    SELECT stock_qty INTO available_stock
+    FROM Tbl_Products
+    WHERE prod_id = NEW.prod_id;
+
+    IF NEW.qty > available_stock THEN
+        RAISE EXCEPTION 'Order quantity exceeds available stock';
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE TRIGGER trg_check_stock
+BEFORE INSERT ON Tbl_Orders
+FOR EACH ROW
+EXECUTE FUNCTION check_stock_before_insert();
